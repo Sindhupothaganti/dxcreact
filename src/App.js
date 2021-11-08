b5cef96d6c855f6f03cafd4f0867890d40bf2d45
Sindhupothaganti <sindhupothaganti17.com>
2021-11-08 16:32:36 +0530

    set state

--- src/App.js
+++ src/App.js
@@ -3,23 +3,27 @@ import './App.css';
 import Greet from './components/Greet';
 import Welcome from './components/Welcome';
 import Message from './components/Message';
+import Counter from './components/Counter';
  
 
 function App() {
   return (
     <div className="App">
+
+    <Counter/>
+
     <Message/>
 
-    <Greet name = "ram"  college = "IIT">
+    <Greet name = "abdul"  college = "IIT">
       <p> this is children property</p>
     </Greet>
 
-    <Greet name = "sham" college = "Oxford">
+    <Greet name = "ansari" college = "Oxford">
       <button>click me</button>
     </Greet>
     <Greet />
 
-    <Welcome name ="sindhu" alias = "sind"/>
+    <Welcome name ="tanveer" alias = "ansari"/>
 
     </div>
   );
