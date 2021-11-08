16426ed84e8743ca90fd4f53e2cfcdd96bb6e90a
Sindhupothaganti <sindhupothaganti17.com>
2021-11-08 15:06:29 +0530

    passing properties

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 import Greet from './components/Greet';
+import Welcome from './components/Welcome';
 
 function App() {
   return (
     <div className="App">
-      <Greet/>
-      <Greet/>
+    <Greet name = "raj"  college = "IIT">
+      <p> this is children property</p>
+      <h1>this is h1</h1>
+    </Greet>
+    <Greet name = "ram" college = "Oxford"/>
+    <Greet name ="sindhu"/>
+
+    <Welcome/>
+
     </div>
   );
 }
--- src/components/Greet.js
+++ src/components/Greet.js
@@ -3,5 +3,11 @@ import React from  'react'
 /* function Greet(){
     return  <h1>welcome to DXC</h1> 
 }*/
-const Greet = () => <h1>hello sindhu</h1>
+const Greet = (props) => {
+    console.log(props);
+    return (
+        <div>
+            <h1> hello  {props.name} you are from {props.college}</h1>
+            {props.children}
+        </div>)}
 export default Greet
