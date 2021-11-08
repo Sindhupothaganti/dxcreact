102d1bb072b6730525f02ae032d2ca37fd392cec
Sindhupothaganti <sindhupothaganti17.com>
2021-11-08 15:42:54 +0530

    state change

--- src/App.js
+++ src/App.js
@@ -2,18 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 import Greet from './components/Greet';
 import Welcome from './components/Welcome';
+import Message from './components/Message';
+ 
 
 function App() {
   return (
     <div className="App">
-    <Greet name = "raj"  college = "IIT">
-      <p> this is children property</p>
-      <h1>this is h1</h1>
-    </Greet>
-    <Greet name = "ram" college = "Oxford"/>
-    <Greet name ="sindhu"/>
+    <Message/>
 
-    <Welcome/>
+    <Greet name = "ram"  college = "IIT">
+      <p> this is children property</p>
+    </Greet>
+
+    <Greet name = "sham" college = "Oxford">
+      <button>click me</button>
+    </Greet>
+    <Greet />
+
+    <Welcome name ="sindhu" alias = "sind"/>
 
     </div>
   );
--- src/components/Greet.js
+++ src/components/Greet.js
@@ -1,13 +1,18 @@
-import React from  'react'
+import React from 'react'
 
 /* function Greet(){
-    return  <h1>welcome to DXC</h1> 
-}*/
+    return  <h1>welcome abdul</h1>
+} */
+
 const Greet = (props) => {
     console.log(props);
+    //props.name = 'dxc'
     return (
         <div>
             <h1> hello  {props.name} you are from {props.college}</h1>
             {props.children}
-        </div>)}
+        </div>
+    )
+
+}
 export default Greet
